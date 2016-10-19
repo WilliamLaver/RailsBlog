b7e6e6531dcabbca67503c6a2d0f3f2ee950cbb6
William Laver <george.laver@mail.mcgill.ca>
2016-10-19 13:39:31 -0600

    added Article controller, updated router and wrote index and show views

--- Blogger/config/routes.rb
+++ Blogger/config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+	get '/articles' => 'articles#index'
+	get '/articles/:id' => 'articles#show', as: :article
+	get '/articles/new' => 'articles#new', as: :new_article
+	post '/articles' => 'articles#create'
+	get '/articles/:id/edit' => 'articles#edit'
+	patch '/articles/:id' => 'articles#update'
+	put '/articles/:id' => 'articles#update'
+	delete '/articles/:id' => 'articles#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
