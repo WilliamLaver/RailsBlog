37e3c438414cef6d1c320cd870354b73393ccdb9
William Laver <george.laver@mail.mcgill.ca>
2016-10-20 14:14:16 -0600

    Completed commenting feature

--- Blogger/config/routes.rb
+++ Blogger/config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
 	root to: 'articles#index'
-	get '/articles' => 'articles#index'
-	post '/articles' => 'articles#create'
-	get '/articles/new' => 'articles#new', as: :new_article
-	get '/articles/:id/edit' => 'articles#edit', as: :edit_article
-	get '/articles/:id' => 'articles#show', as: :article
-	patch '/articles/:id' => 'articles#update'
-	put '/articles/:id' => 'articles#update'
-	delete '/articles/:id' => 'articles#destroy'
+#	get '/articles' => 'articles#index'
+#	post '/articles' => 'articles#create'
+#	get '/articles/new' => 'articles#new', as: :new_article
+#	get '/articles/:id/edit' => 'articles#edit', as: :edit_article
+#	get '/articles/:id' => 'articles#show', as: :article
+#	patch '/articles/:id' => 'articles#update'
+#	put '/articles/:id' => 'articles#update'
+#	delete '/articles/:id' => 'articles#destroy'
+	resources :articles do 
+		resources :comments
+	end
 	
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
