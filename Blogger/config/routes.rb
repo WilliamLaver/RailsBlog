17d7254e753b3d00b11e42d61905de40ebee59d2
William Laver <george.laver@mail.mcgill.ca>
2016-10-23 21:34:21 -0600

    Added tags to articles with minimal functionality for now, need to implement an index page to list all tags along with the ability to remove tags from articles

--- Blogger/config/routes.rb
+++ Blogger/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 	root to: 'articles#index'
+# Following commented lines describe a hardcoded 'resources' call:
 #	get '/articles' => 'articles#index'
 #	post '/articles' => 'articles#create'
 #	get '/articles/new' => 'articles#new', as: :new_article
@@ -11,6 +12,7 @@ Rails.application.routes.draw do
 	resources :articles do 
 		resources :comments
 	end
+	resources :tags
 	
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
