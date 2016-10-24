cafe77ae0cbc028dc12c1cef8ebf040e4051f530
William Laver <george.laver@mail.mcgill.ca>
2016-10-24 17:22:41 -0600

    Completed tag functionality by adding index page and delete option

--- Blogger/app/controllers/tags_controller.rb
+++ Blogger/app/controllers/tags_controller.rb
@@ -4,12 +4,24 @@ class TagsController < ApplicationController
 		@tag = Tag.find(params[:id])
 	end
 
+	def index
+		@tags = Tag.all
+	end
+
 	before_filter :require_login, only: [:destroy]
 
 	def require_login
 		unless logged_in?
-			alert("Cannot delete tags unless logged in!")
-			redirect_to('show')
+			redirect_back fallback_location: '/tags'
+			flash.notice = "Cannot delete tags unless logged in!"
+			return false
 		end
 	end
+
+	def destroy
+		@tag = Tag.find(params[:id])
+		@tag.destroy
+		redirect_to '/tags'
+	end
+
 end
